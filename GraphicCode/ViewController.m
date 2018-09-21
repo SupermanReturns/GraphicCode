1930909ce5cb98daf819a9e3c0c82bff1e45713b
Superman <703353105@qq.com>
2018-09-21 11:57:44 +0800

    修改代码

--- GraphicCode/ViewController.m
+++ GraphicCode/ViewController.m
@@ -20,14 +20,7 @@
     // Do any additional setup after loading the view, typically from a nib.
     CGSize size = self.view.bounds.size;
     
-    //    //native image to detect
-    //    UIImage *img__ = [UIImage imageNamed:@"test_2.jpg"];
-    //    NHGraphCoder *coder = [NHGraphCoder codeWithImage:img__];
-    //    coder.center = CGPointMake(size.width*0.5, size.height*0.5);
-    //    [coder handleGraphicCoderVerifyEvent:^(NHGraphCoder * _Nonnull cd, BOOL success) {
-    //        NSLog(@"验证结果:%d",success);
-    //    }];
-    //    [self.view addSubview:coder];
+
     
     //network image to detect
     NSString *url = @"http://pic.pimg.tw/loloto/1357207442-1350656755_l.jpg?v=1357207447";
